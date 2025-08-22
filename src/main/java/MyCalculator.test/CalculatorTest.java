290f6c34bc2ca7ada613efcb62e53eb712ba0387
Enmanuel-caraballo <enmanuelcm03@gmail.com>
2025-08-22 00:12:50 -0500

    arreglos tests +1

--- src/main/java/MyCalculator.test/CalculatorTest.java
+++ src/main/java/MyCalculator.test/CalculatorTest.java
@@ -1,12 +1,29 @@
 package MyCalculator.test;
-
+import org.example.Main;
 import MyCalculator.Calculadora;
 import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
+
 
 public class CalculatorTest {
     private final Calculadora clc = new Calculadora();
 
+    @Test
+    public void  VerificarEntradaValidaTest(){
+        String entradaSimulada = "abc\n5\n6\n";
+        InputStream input  = new ByteArrayInputStream(entradaSimulada.getBytes());
+        System.setIn(input);
+
+        try{
+          Main.main(new  String[]{});
+
+        } catch (Exception e) {
+            fail("El programa no deberia salir");
+        }
+    }
     @Test
     public void testSumar(){
         assertEquals(clc.sumar(1,2),3);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import MyCalculator.Calculadora;
 
@@ -7,26 +8,43 @@ import MyCalculator.Calculadora;
 public class Main {
 
     static Scanner lector = new Scanner(System.in);
-    static int op;
+    static int op, a = 0, b = 0;
     static int result = 0;
+
 
     public static void main(String[] args) {
 
         Calculadora clc = new Calculadora();
+        boolean valueOk = false;
 
-        System.out.println("Digite el primer numero (a): ");
-        int a = lector.nextInt();
-        System.out.println("Digite el segundo numero (b): ");
-        int b = lector.nextInt();
+        while (!valueOk) {
+        try{
+
+            System.out.println("Digite el primer numero (a): ");
+             a = lector.nextInt();
+            System.out.println("Digite el segundo numero (b): ");
+             b = lector.nextInt();
+            valueOk = true;
+        } catch (InputMismatchException e) {
+            System.out.println("Ingrese una opcion valida");
+            lector.nextLine();
+        }
+
+        }
+
 
         try{
-            Thread.sleep(1000);
+            Thread.sleep(500);
 
         }catch (InterruptedException e){
             e.printStackTrace();
         }
 
-        menu();
+        do {
+            menu();
+
+        try {
+
         op = lector.nextInt();
 
         switch (op){
@@ -60,6 +78,16 @@ public class Main {
                 break;
         }
 
+
+
+        } catch (InputMismatchException e) {
+            if(Character.isDigit(op) || op > 4)
+            System.out.println("Ingrese una opcion valida");
+            lector.nextLine();
+        }
+
+        }while (op != 0);
+
     }
 
     public static void menu(){
@@ -70,5 +98,6 @@ public class Main {
         System.out.println("2.Restar");
         System.out.println("3.Multiplicar");
         System.out.println("4.Dividir");
+        System.out.println("0.Salir");
     }
 }
